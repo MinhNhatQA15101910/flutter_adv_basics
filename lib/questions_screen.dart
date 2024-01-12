07f060c4066f0c3d6bd4a9d820e2bfb979eb9d13
Do Nhat <dmnhat.pt@gmail.com>
2024-01-12 11:07:33 +0700

    Using third-party packages and adding google fonts.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -34,8 +35,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,
+              style: GoogleFonts.lato(
+                color: Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen(this.startQuiz, {super.key});
@@ -17,11 +18,11 @@ class StartScreen extends StatelessWidget {
             color: const Color.fromARGB(150, 255, 255, 255),
           ),
           const SizedBox(height: 80),
-          const Text(
+          Text(
             'Learn Flutter the fun way!',
-            style: TextStyle(
+            style: GoogleFonts.lato(
               fontSize: 24,
-              color: Color.fromARGB(255, 237, 223, 252),
+              color: const Color.fromARGB(255, 237, 223, 252),
             ),
           ),
           const SizedBox(height: 30),
