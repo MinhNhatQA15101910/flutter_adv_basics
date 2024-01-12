952555366d47d7dd0dfaba97aa04ca6a6fb76002
Do Nhat <dmnhat.pt@gmail.com>
2024-01-12 10:20:59 +0700

    Mapping lists and using the spread operator.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -28,22 +28,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
           ),
           const SizedBox(height: 30),
-          AnswerButton(
-            answerText: currentQuestion.answers[0],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[1],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[2],
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: currentQuestion.answers[3],
-            onTap: () {},
-          ),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {},);
+          })
         ],
       ),
     );
