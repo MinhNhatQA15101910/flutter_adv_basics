8a9f5167af6f39b0972b9f1c166eca8ed213d264
Do Nhat <dmnhat.pt@gmail.com>
2024-01-12 09:07:22 +0700

    Using ternary expressions and comparison operators.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,18 +12,12 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
-  }
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
   }
 
   @override
@@ -41,7 +35,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
         ),
       ),
     );
