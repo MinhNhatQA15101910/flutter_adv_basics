04f355364f50e5912d38b45aff2458626163b3e7
Do Nhat <dmnhat.pt@gmail.com>
2024-01-12 08:02:08 +0700

    Adding icons to buttons

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -22,12 +22,13 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 30),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: const Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
           )
         ],
       ),
