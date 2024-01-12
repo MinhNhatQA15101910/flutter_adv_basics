a88401d56eaf4f11900b442effd29f59f768b0ad
Do Nhat <dmnhat.pt@gmail.com>
2024-01-12 09:39:08 +0700

    Configuring a column.

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -1,5 +1,5 @@
 class QuizQuestion {
-  const  QuizQuestion(this.text, this.answers);
+  const QuizQuestion(this.text, this.answers);
 
   final String text;
   final List<String> answers;
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,6 +12,31 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...'),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1...'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2...'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3...'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4...'),
+          ),
+        ],
+      ),
+    );
   }
 }
