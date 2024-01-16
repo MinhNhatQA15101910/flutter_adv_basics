606e37a032541fc6a9b3819ac6afbe54038e2c9a
Do Nhat <dmnhat.pt@gmail.com>
2024-01-16 13:22:16 +0700

    Making content scrollable with SingleChildScrollView.

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -7,24 +7,29 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map((data) {
-        return Row(
-          children: [
-            Text(((data['question_index'] as int) + 1).toString()),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(data['question'] as String),
-                  const SizedBox(height: 5),
-                  Text(data['user_answer'] as String),
-                  Text(data['correct_answer'] as String),
-                ],
-              ),
-            )
-          ],
-        );
-      }).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map((data) {
+            return Row(
+              children: [
+                Text(((data['question_index'] as int) + 1).toString()),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Text(data['question'] as String),
+                      const SizedBox(height: 5),
+                      Text(data['user_answer'] as String),
+                      Text(data['correct_answer'] as String),
+                    ],
+                  ),
+                )
+              ],
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
