6a17313856ee825689c60af430f3466d24add7ca
Do Nhat <dmnhat.pt@gmail.com>
2024-01-16 10:54:15 +0700

    Accessing map values and using "type casting".

---
+++ lib/questions_summary.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summaryData.map((data) {
+        return Row(
+          children: [
+            Text(((data['question_index'] as int) + 1).toString()),
+          ],
+        );
+      }).toList(),
+    );
+  }
+}
