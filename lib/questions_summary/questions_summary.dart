fe5dd9b0083706d7a0de74947014ea40a936e223
Do Nhat <dmnhat.pt@gmail.com>
2024-01-16 14:43:23 +0700

    Finish app.

--- lib/questions_summary/questions_summary.dart
+++ lib/questions_summary/questions_summary.dart
@@ -9,7 +9,7 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 400,
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
--- lib/questions_summary/summary_item.dart
+++ lib/questions_summary/summary_item.dart
@@ -12,55 +12,58 @@ class SummaryItem extends StatelessWidget {
     final isCorrectAnswer =
         itemData['user_answer'] == itemData['correct_answer'];
 
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        // Index identifier
-        QuestionIdentifier(
-          questionIndex: itemData['question_index'] as int,
-          isCorrectAnswer: isCorrectAnswer,
-        ),
-
-        // Blank space with width = 20
-        const SizedBox(width: 20),
-
-        // Question information
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Question text
-              Text(
-                itemData['question'] as String,
-                style: GoogleFonts.lato(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              // Blank space with height = 5
-              const SizedBox(height: 5),
-
-              // User answer
-              Text(
-                itemData['user_answer'] as String,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 202, 171, 252),
-                ),
-              ),
-
-              // Correct answer
-              Text(
-                itemData['correct_answer'] as String,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 181, 254, 246),
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Index identifier
+          QuestionIdentifier(
+            questionIndex: itemData['question_index'] as int,
+            isCorrectAnswer: isCorrectAnswer,
           ),
-        ),
-      ],
+
+          // Blank space with width = 20
+          const SizedBox(width: 20),
+
+          // Question information
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Question text
+                Text(
+                  itemData['question'] as String,
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                // Blank space with height = 5
+                const SizedBox(height: 5),
+
+                // User answer
+                Text(
+                  itemData['user_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 202, 171, 252),
+                  ),
+                ),
+
+                // Correct answer
+                Text(
+                  itemData['correct_answer'] as String,
+                  style: const TextStyle(
+                    color: Color.fromARGB(255, 181, 254, 246),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
